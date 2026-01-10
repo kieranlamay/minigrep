62d9c1403876f7ee897fb1d878d9fe6673864fe7
kieranlamay <kieran_lamay@brown.edu>
2026-01-10 00:18:01 -0500

    feat: basic CLI argument intake

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::env;
+use std::process;
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn configure(query: String, filename: String) -> Config {
+    Config { query, filename }
+}
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    if args.len() < 3 {
+        println!("Error: Too few arguments");
+        process::exit(1);
+    } else if args.len() > 3 {
+        println!("Error: Too many arguments")
+        process::exit(1);
+    }
+    args.next();
+    
+    let Some(query) = args.next() else {
+        process::exit(1);
+    };
+
+    let Some(filename) = args.next() else {
+        process::exit(1);
+    };
+    let config : Config = configure(query, filename);
+
+    println!("Query: {}, Filename: {}", &config.query, &config.filename);
 }
